7bf17252e675d60b68c7c76d930f567464264003
sophiecomputer <sophie.computer.100@gmail.com>
2026-06-06 21:24:39 -0500

    markdown2html, badge

---
+++ markdown2html.py
@@ -0,0 +1,455 @@
+"""
+Converts a markdown file to HTML. There are two conversion modes:
+
+1. Does a direct translation of the markdown to an HTML file.
+2. Prepends/appends content related to a certain target format (i.e., blog 
+   posts).
+
+The user supplies the path to an *output directory* where the "index.html" 
+should be written *alongside* any media file. The input markdown file may 
+contain media in the form "![[/path/to/media]]" (relative to the input 
+directory). This content is copied to a "media/" directory and the resulting 
+"index.html" file includes it. Finally, the resulting 
+"""
+
+import argparse
+import os
+import re
+import shutil 
+import sys 
+
+from datetime import datetime, UTC 
+from pathlib import Path 
+from typing import Optional 
+
+
+def get_args():
+    def valid_md(arg):
+        if not os.path.exists(arg):
+            raise argparse.ArgumentTypeError(f"Path \"{arg}\" does not exist.") 
+        if not arg.endswith(".md"):
+            raise argparse.ArgumentTypeError(
+                f"Path \"{arg}\" is not a Markdown file.")
+        return os.path.abspath(arg)
+
+    args = argparse.ArgumentParser()
+    args.add_argument("inputpath", type=valid_md, 
+        help="Path to the input \".md\" file")
+    args.add_argument("outputpath",
+        help="Path to where the output directory should be written")
+    args.add_argument("--fmt", default="plain", choices=["plain", "blog"],
+        help="The format of the outputted HTML")
+    args.add_argument("--title", default="SophieComputer Article", 
+        help="The title of the outputted HTML page")
+    args.add_argument("--webdomain", default="https://sophie.computer", 
+        help="The domain of the website (default = https://sophie.computer")
+    args.add_argument("--wikilink-root", default=None,
+        help=("The root directory of where Wiki-style links (i.e., "
+              "\"![[foo]]\" should search content for)"))
+    args.add_argument("--overwrite", action="store_true", 
+        help=("If supplied, overwrites the target directory if it exists "
+              "without asking user for permission"))
+
+    return args.parse_args() 
+
+
+def find_git_root(path: str):
+    """
+    Traces upwards from the path until a ".git" directory is found. For example,
+    if "foo/bar/baz/qux" is given and "foo/bar/.git" exists, then returns 
+    "foo/bar".
+    """
+
+    path = Path(path).resolve() 
+    
+    # If a file was provided, start from its parent directory. 
+    current = path if path.is_dir() else path.parent 
+
+    # Trace upwards. 
+    while True:
+        if (current / ".git").is_dir():
+            return str(current) 
+        
+        if current.parent == current:
+            raise FileNotFoundError((
+                f"Error: no parent directory from \"{str(path)}\" contains a "
+                f"\".git\" directory."
+            ))
+
+        current = current.parent 
+
+
+def find_file(fname: str, root_path: str): 
+    """
+    Finds the file with the given name starting from the root directory. If 
+    multiple files have the name, raise an error. 
+    """
+
+    matches = [p for p in Path(root_path).rglob(fname) if p.is_file()]
+    if len(matches) == 0:
+        raise FileNotFoundError(f"{fname} can't be found under {root_path}") 
+    if len(matches) > 1: 
+        raise ValueError(
+            f"Multiple files with name {fname} found under {root_path}"
+        )
+    return matches[0] 
+
+
+def blog_format_start(title: str, relative_index: str):
+    """
+    The start of the blog format. "relative_index" must be relative to the base
+    directory of the website (i.e., it must begin with a "/").
+    """
+
+    assert (
+        relative_index.startswith("/") and 
+        relative_index.endswith("index.html")
+    ), relative_index
+
+    return f"""
+        <!DOCTYPE html>
+        <html>
+          <head>
+            
+            <meta charset="UTF-8">
+            <meta name="viewport" 
+              content="width=device-width, initial-scale=1.0">
+            <title>{title}</title>
+            <link href="/style-content.css" rel="stylesheet" type="text/css" 
+              media="all">
+          
+          </head>
+          <body>
+            
+            <script> 
+              const query_string = window.location.search; 
+              const url_params = new URLSearchParams(query_string); 
+              if (!url_params.has('contentonly')) {{
+                window.location.replace("/index.html?redirect={relative_index}");
+              }}
+            </script> 
+        
+            <div style="width: 100%%; height: 100%%;">
+              <div class="main-content">
+                <div class="main-section">
+    """
+
+
+def blog_format_end():
+    timestamp = datetime.now(UTC).strftime(
+        "Last updated: %B %-d, %Y at %H:%M UTC"
+    )
+    return f"""
+        </div></div>
+        <div class=\"last_updated\">
+          <p class=\"last-updated-text\">
+            {timestamp}
+          </p>
+        </div>
+        </div></body></html>
+    """
+
+
+def convert(
+    input_path: str, 
+    output_path: str, 
+    title: str, 
+    force_overwrite: bool,
+    fmt: str,
+    domain: str,
+    wikilink_root: Optional[str] 
+) -> None:
+    """
+    Converts the input path (a markdown file) to an HTML file at 
+    "{output_path}/index.html". If the markdown file contains any media, then 
+    copies that media to "{output_path}/media/*".
+
+    We can specify a target file format. The format can be either "plain" or 
+    "blog".
+
+    "domain" is the domain of the website (i.e., "https://example.com"). 
+    """
+    
+    assert fmt in ("plain", "blog"), fmt 
+
+    if os.path.exists(output_path): 
+        if not force_overwrite: 
+            option = input(
+                f"The path \"{output_path}\" already exists. Delete it? (y/N): "
+            ).strip().lower() 
+            
+            if option not in ("y", "yes"):
+                print("Exiting.")
+                exit(0)
+            
+        # Delete the path. 
+        print(f"Deleting \"{output_path}\"") 
+        shutil.rmtree(output_path)
+    
+    # Make the output directory. 
+    os.makedirs(output_path)
+    output_fname = f"{output_path}/index.html" 
+
+    # The file is created in a streaming fashion.
+    with open(input_path, "r") as f_in: 
+        with open(output_fname, "w") as f_out:
+            # Depending on the target file format, write a prelude to the file.
+            if fmt == "plain":
+                f_out.write("<html>")
+            elif fmt == "blog":
+                # The user supplied an "output_path" describing where to write 
+                # the file. Turn this into an absolute path, and find where the
+                # ".git" root is. We're assuming this blog is represented as a
+                # GitHub Pages.
+                output_path = os.path.abspath(output_path)
+                root = find_git_root(os.path.abspath(output_path))
+                rel_path = output_path[len(root):] + "/index.html"
+                f_out.write(blog_format_start(title, rel_path))
+            else:
+                raise ValueError(f"Unknown format \"{fmt}\"") 
+
+            # Some markdown elements extend multiple lines. For example:
+            #   - code blocks ("code_block")
+            #   - bulleted lists ("bullet_list") 
+            #   - numbered lists ("number_list") 
+            # For each of these, we must know if we're currently reading 
+            # something that extends multiple lines. For the *first line* that 
+            # matches this description, emit the start of the block and the 
+            # first line. For the first line that *doesn't* match this format, 
+            # emit the end of the block. 
+            multiline = None
+            
+            # Process the file.
+            for line in f_in:
+                line = line.strip()
+                
+                # Assume the line is to be written as a new paragraph <p>. We
+                # do *not* emit this as a new paragraph if the line is anything
+                # multiline or if it's a header. 
+                is_paragraph = True
+
+                # Match code blocks.
+                if "```" in line:
+                    assert line == "```"  # Nothing else besides ```. 
+                    assert multiline == "code_block" or multiline is None
+                    if multiline is None:
+                        # Emit the beginning of the code block.
+                        f_out.write("<div class=\"code-block\"><pre><code>")
+                        multiline = "code_block" 
+                    else:
+                        # Emit the end of the code block.
+                        f_out.write("</code></pre></div>")
+                 
+                # The above may have made the line contain no text. Ignore empty 
+                # lines. 
+                if len(line) == 0:
+                    continue
+
+                # Match bulleted lists. 
+                if line.startswith("* "):
+                    assert multiline == "bullet_list" or multiline is None
+                    if multiline is None:
+                        # Emit the beginning of the bullet list. 
+                        f_out.write("<ul>") 
+                        multiline = "bullet_list" 
+                    
+                    # We're currently emitting a bullet list. 
+                    line = line[2:]  # Remove "* "
+                elif multiline == "bullet_list":
+                    # We're not matching a bullet list, but we previously were. 
+                    # Emit the end of the bullet list. 
+                    f_out.write("</ul>") 
+                    multiline = None 
+                
+                # Match numbered lists.
+                if re.match(r"^\d+\. ", line):
+                    assert multiline == "number_list" or multiline is None
+                    if multiline is None:
+                        # Emit the beginning of the number list. 
+                        f_out.write("<ol>") 
+                        multiline = "number_list" 
+                    
+                    # We're currently emitting a number list.
+                    line = line[line.rindex(" ") + 1:]  # Remove number 
+                elif multiline == "number_list":
+                    # We're not matching a number list, but we previously were. 
+                    # Emit the end of the number list. 
+                    f_out.write("</ol>") 
+                    multiline = None
+                
+                # Copy images. Replace the line with an image tag, and copy the 
+                # input image to the output. (Assert this is actually an image, 
+                # too.)
+                if mat := re.match(r"!\[\[([^\]]+)\]\]", line):
+                    # This is a Wiki-style link. Search for the file name 
+                    # relative to the Wiki-link root.
+                    name = mat[1] 
+                    assert any(
+                        name.endswith(ext) 
+                        for ext in (".png", ".jpg", ".jpeg", ".svg")
+                    ), name
+                    assert "/" not in name, name  # Adjust code to work for this
+                    assert wikilink_root is not None
+                    path = os.path.abspath(find_file(name, wikilink_root))
+                    
+                    # Copy file to target directory. 
+                    if not os.path.exists(f"{output_path}/media"):
+                        os.makedirs(f"{output_path}/media") 
+                    dest = f"{output_path}/media/{name}"
+                    shutil.copyfile(path, dest)
+                    print(f"Copied \"{path}\" to \"{dest}\"")
+
+                    line = (
+                        "<center><img "
+                        + f"src=\"./media/{name}\" "
+                        + "style=\"width: 70%; height: auto;\" "
+                        + "class=\"center\"></center>"
+                    )
+                
+                if mat := re.match(r"!\[([^\]]+)\]\(([^\)]+)\)", line):
+                    # Assert the content is an image. 
+                    alt = mat[1].strip()
+                    link = mat[2].strip() 
+                    assert any(
+                        link.endswith(ext) 
+                        for ext in (".png", ".jpg", ".jpeg", ".svg")
+                    ), f"\"{alt}\", \"{link}\""
+
+                    # If the link is a web hyperlink (beginning with "http://" 
+                    # or "https://"), it does not need to be copied anywhere. 
+                    # Otherwise, it must be pointed to a file.
+                    if not (
+                        link.startswith("http://") or 
+                        link.startswith("https://")
+                    ):
+                        assert os.path.exists(link), link
+
+                        # The file exists, so copy it to the target directory.
+                        # Get the base name. 
+                        basename = link[link.rfind("/")+1:].lstrip("/")
+                        target_name = f"{output_path}/media/{basename}"
+                        if not os.path.exists(f"{output_path}/media"):
+                            os.makedirs(f"{output_path}/media") 
+                        shutil.copyfile(link, f"{output_path}/media/{basename}")
+                        print(f"Copied \"{link}\" to \"{target_name}\"")
+                        link = f"./media/{basename}"
+
+                    line = (
+                        "<center><img " 
+                        + f"src=\"{link}\" "
+                        + f"alt=\"{alt}\" "
+                        + "style=\"width: 70%; height: auto;\" "
+                        + "class=\"center\"></center>"
+                    )
+
+                # Replace inline markdown elements in the line with their HTML
+                # equivalents. Note that markdown elements can be repeated on 
+                # the same line (i.e., "hello *sophie* super *computer*" 
+                # contains two italics). First, substitute triple asterisks with 
+                # bold + italics.
+                while mat := re.search(r"\*\*\*([^\*]+)\*\*\*", line):
+                    line = (
+                        line[:mat.start()]
+                        + "<b><i>"
+                        + mat[1]
+                        + "</i></b>"
+                        + line[mat.end():]
+                    )
+                
+                # Substitute double asterisks with bold. 
+                while mat := re.search(r"\*\*([^\*]+)\*\*", line):
+                    line = (
+                        line[:mat.start()]
+                        + "<b>"
+                        + mat[1]
+                        + "</b>"
+                        + line[mat.end():]
+                    )
+                
+                # Substitute single asterisks with italics. 
+                while mat := re.search(r"\*([^\*]+)\*", line):
+                    line = (
+                        line[:mat.start()]
+                        + "<i>"
+                        + mat[1]
+                        + "</i>"
+                        + line[mat.end():]
+                    )
+
+                # Substitute backticks with inline code. 
+                while mat := re.search(r"`([^`]+)`", line):
+                    line = (
+                        line[:mat.start()]
+                        + "<code>"
+                        + mat[1]
+                        + "</code>"
+                        + line[mat.end():]
+                    )
+
+                # Substitute links.
+                while mat := re.search(r"(\[[^\]]+\])\(([^\)]+)\)", line): 
+                    link = mat[2] 
+                    text = mat[1] 
+                    
+                    # Open a new tab if the target URL is not within our own 
+                    # website.
+                    target = "target=\"_blank\"" if domain not in link else ""
+
+                    line = (
+                        line[:mat.start()]
+                        + f"<a href=\"{link}\" {target}>{text}</a>"
+                        + line[mat.end():]
+                    )
+                
+                # Next, modify markdown elements that change the behavior of the
+                # entire line. These include bullet/number lists and headers. 
+                # Modify the line to include these.
+                if multiline in ("bullet_list", "number_list"):
+                    line = f"<li>{line}</li>"
+                
+                headers = {
+                    "#####": "h5", 
+                    "####" : "h4", 
+                    "###"  : "h3", 
+                    "##"   : "h2", 
+                    "#"    : "h1"
+                }
+                for header, html in headers.items():
+                    if line.startswith(header): 
+                        line = (
+                            f"<{html}>"
+                            + line[len(header):].lstrip()
+                            + f"</{html}>"
+                        )
+                        is_paragraph = False 
+                        break 
+                
+                # Do we write this as a paragraph <p>?
+                if is_paragraph and multiline is None: 
+                    line = f"<p>{line}</p>" 
+
+                # Write the final line.
+                f_out.write(line) 
+            
+            # Depending on the target file format, write an ending to the file.
+            if fmt == "plain":
+                f_out.write("</html>")
+            elif fmt == "blog":
+                f_out.write(blog_format_end())
+            else:
+                raise ValueError(f"Unknown format \"{fmt}\"") 
+    
+    print(f"HTML output written to \"{output_fname}\"") 
+
+
+if __name__ == "__main__":
+    args = get_args() 
+    convert(
+        input_path=args.inputpath, 
+        output_path=args.outputpath, 
+        title=args.title, 
+        force_overwrite=args.overwrite, 
+        fmt=args.fmt,
+        domain=args.webdomain,
+        wikilink_root=args.wikilink_root
+    )
